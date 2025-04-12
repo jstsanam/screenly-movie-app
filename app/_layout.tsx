b0c683e8c19573edffb94c54603c7ef2255e4a63
Sanam Yadav <jstsanam@gmail.com>
2025-04-12 20:20:34 +0530

    Routes added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,25 @@
 import { Stack } from "expo-router";
 import "./global.css";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <StatusBar hidden={true} />
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="movie/[id]"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </>
+  );
 }
